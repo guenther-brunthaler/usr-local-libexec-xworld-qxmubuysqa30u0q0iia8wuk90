f535f96c873b5b166228c447b923122fcd074b6c
Guenther Brunthaler <root@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2013-04-04 01:24:18 +0200

    Added --stop support for path_helper_hyec3v5m8kd1vjs8k7d1wce62
    This is required by dash which does not support positional parameters
    for the "." command.

--- sh/path_hyec3v5m8kd1vjs8k7d1wce62.sh
+++ sh/path_hyec3v5m8kd1vjs8k7d1wce62.sh
@@ -1,9 +1,13 @@
 #! /bin/false
 # Source this snippet for modifying $PATH.
 #
-# The arguments may consist of any number of
-# action statements, they will all be performed in the
-# order specified.
+# The arguments may consist of any number of action statements, they will all
+# be performed in the order specified.
+#
+# Note that passing arguments to the "." command is not* portable!
+#
+# Use the "set" command to set the arguments for sourcing this script, use the
+# --stop option to save old arguments after it.
 #
 # Supported action statements:
 #
@@ -15,14 +19,23 @@
 #   Insert <path> before the beginning of $PATH, but only if <path> is
 #   actually an existing directory and if it is not already part of $PATH.
 #
-# written in 2008 by Guenther Brunthaler
+# Options only valid after all action statements:
+#
+# --stop
+#   Stops argument processing, leaving the remaining arguments set as $1 and
+#   onward. This is needed if it is required to "save" previous arguments
+#   after the --stop, which will become the current arguments again after
+#   sourcing this script. Typically used as '--stop "$@"'.
+#
+# written in 2008 - 2013 by Guenther Brunthaler
 
 
 # Process a single action statement.
 path_helper_hyec3v5m8kd1vjs8k7d1wce62() {
 	local TARGET RPATH PCOMP OPT_FRONT
 	OPT_FRONT=; TARGET=
-	while test $# != 0; do
+	while test $# != 0
+	do
 		case $1 in
 			--prepend) OPT_FRONT=1; TARGET=$2; shift;;
 			--append) TARGET=$2; shift;;
@@ -32,13 +45,15 @@ path_helper_hyec3v5m8kd1vjs8k7d1wce62() {
 	done
 	test ! -d "$TARGET" && return
 	RPATH=$PATH
-	while true; do
+	while true
+	do
 		PCOMP=${RPATH%%:*}
 		test x"$PCOMP" = x"$TARGET" && return
 		test x"$PCOMP" = x"$RPATH" && break
 		RPATH=${RPATH#$PCOMP:}
 	done
-	if test -n "$OPT_FRONT"; then
+	if test -n "$OPT_FRONT"
+	then
 		PATH=$TARGET${PATH:+:}$PATH
 	else
 		PATH=$PATH${PATH:+:}$TARGET
@@ -49,13 +64,15 @@ path_helper_hyec3v5m8kd1vjs8k7d1wce62() {
 
 # Process a list of action statements.
 path_hyec3v5m8kd1vjs8k7d1wce62() {
-	while test $# != 0; do
+	while test $# != 0
+	do
 		case $1 in
 			--append | --prepend)
 				path_helper_hyec3v5m8kd1vjs8k7d1wce62 \
 					"$1" "$2"
 				shift
 				;;
+			--stop) break;;
 			*)
 				echo "Warning: Ignored arguments >>>$*<<<" \
 					>& 2
@@ -69,5 +86,13 @@ path_hyec3v5m8kd1vjs8k7d1wce62() {
 
 # Avoid namespace pollution and symbol clashes.
 path_hyec3v5m8kd1vjs8k7d1wce62 "$@"
+while test $# != 0
+do
+	if test x"$1" = x"--stop"
+	then
+		shift; break
+	fi
+	shift
+done
 unset -f path_hyec3v5m8kd1vjs8k7d1wce62 \
 	path_helper_hyec3v5m8kd1vjs8k7d1wce62
